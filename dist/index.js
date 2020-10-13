240a02b8354b789a2ff505f25ada052078dd9768
Daniel <daniru9@gmail.com>
2020-10-13 18:11:14 +0200

    mongoose añadido

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./classes/server"));
+const usuario_1 = __importDefault(require("./routes/usuario"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const server = new server_1.default();
+//Rutas de mi app
+server.app.use('/user', usuario_1.default);
+//Conectar BBDD
+mongoose_1.default.connect('mongodb://localhost:27017/iSalud', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err) => {
+    if (err)
+        throw err;
+    console.log('Base de datos online');
+});
 //Levantar express
 server.start(() => {
     console.log(`Servidor corriendo en puerto ${server.port}`);
--- index.ts
+++ index.ts
@@ -1,7 +1,21 @@
 import Server from './classes/server';
+import userRoutes from './routes/usuario';
+import mongoose from 'mongoose';
 
 
 const server = new Server();
+
+//Rutas de mi app
+server.app.use('/user', userRoutes)
+
+//Conectar BBDD
+mongoose.connect('mongodb://localhost:27017/iSalud', 
+                {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}, (err) => {
+                    if(err) throw err;
+
+                    console.log('Base de datos online');
+                    
+                });
 
 //Levantar express
 server.start(() => {
